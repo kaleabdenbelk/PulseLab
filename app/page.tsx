b3721ed2702f8bf75c4bc49f687df890e6de5b11
kaleabdenbell <kaleab.ugr-97777-18@aau.edu.et>
2026-03-17 18:31:43 +0300

    book a call

--- app/page.tsx
+++ app/page.tsx
@@ -6,11 +6,13 @@ import { Process } from "@/components/Process";
 import { Results } from "@/components/Results";
 import { CTA } from "@/components/CTA";
 import { Footer } from "@/components/Footer";
+import { ClientFlowProvider } from "@/components/ClientFlowContext";
+import { ClientFlowModal } from "@/components/ClientFlowModal";
 
-export default function Home() {
+export default function App() {
   return (
-    <>
-    <GlobalStyles />
+    <ClientFlowProvider>
+      <GlobalStyles />
       <div style={{ background: "#050508", minHeight: "100vh" }}>
         <Navbar />
         <Hero />
@@ -20,6 +22,7 @@ export default function Home() {
         <CTA />
         <Footer />
       </div>
-      </>
+      <ClientFlowModal />
+    </ClientFlowProvider>
   );
 }
